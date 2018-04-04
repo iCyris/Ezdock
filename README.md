c470ea16e410b6cabb1e6af6e2e426d505d7d22f
iCyris <cyris1015@gmail.com>
2018-04-05 00:43:33 +0800

    v1.0

--- README.md
+++ README.md
@@ -2,20 +2,13 @@
 ## Introduction
 A simple PHP development environment for Docker.
 
-This Docker setup includes PHP7-FPM , MySQL and Njinx.
+This Docker setup includes PHP7-FPM , MySQL, Njinx and phpMyAdmin.
 
-Reference: [This page](http://geekyplatypus.com/dockerise-your-php-application-with-nginx-and-php7-fpm/)
-
-## Start
+## Run the application
 1. Put your code into the `code` file
-2. Use `./start.sh`
-3. Take a look at `localhost:8080`
-4. Use `./stop.sh` to stop
-
-## Some useful command
-
-## PhpStorm with Docker
-PhpStorm settings on MacOS
-
-
+2. Start your program: `./start.sh`
+3. Open your browser:
+    * localhost:8080 
+    * localhost:8081    phpMyAdmin (username: root, password: root)
+4. Stop and clear services: `./stop.sh`
 
--- code/index.php
+++ code/index.php
@@ -1,5 +1,24 @@
-<?php
-echo "hello\n";
-echo "<br/>";
-echo "this is a test\n";
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <?php
+            $servername = "127.0.0.1";
+            $username = "root";
+            $password = "root";
+ 
+            try {
+                $conn = new \PDO('mysql:host=mysql;port=3306;dbname=mysql', 'root', 'root');
+                echo "Success! Enjoying coding now."; 
+                // Maybe you need to wait for some seconds for building the database at the first time.
+            }
+            catch(PDOException $e)
+                {
+                    echo $e->getMessage();
+                }
+            ?>
+    </body>
+</html>
